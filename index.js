9a5b0af13ce9c52abae246d00020055efa7e5f5d
TheoPatatoe <46384927+TheoPatatoe@users.noreply.github.com>
2019-01-04 15:20:03 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const botconfig = require("./botconfig.json");
+const Discord = require ("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity("Zvetro on Soundcloud");
+})
+//talking
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if(cmd === `${prefix}a`){
+    return message.channel.send("you fucking coon");
+  }
+
+})
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if(cmd === `${prefix}atwerk`){
+    return message.channel.send("https://www.youtube.com/watch?v=E6dlQDCKVuc&has_verified=1");
+  }
+
+})
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if(cmd === `${prefix}er`){
+    return message.channel.send("Fuck niggers");
+  }
+
+})
+
+bot.login(botconfig.token);
